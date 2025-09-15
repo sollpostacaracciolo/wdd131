1726387b4104693a44728c3b11b52e5bc3a8215d
sollpostacaracciolo <jazzcaracciolo@gmail.com>
2025-09-15 11:05:35 -0300

    ,,.l

--- temples.js
+++ temples.js
@@ -2,25 +2,13 @@ const hamburger = document.getElementById('hamburger');
 const navMenu = document.querySelector('nav ul');
 
 hamburger.addEventListener('click', () => {
-  if (navMenu.style.display === 'none' || navMenu.style.display === '') {
-    navMenu.style.display = 'block';
-  } else {
-    navMenu.style.display = 'none';
-  }
+  navMenu.classList.toggle('show');
 });
 
-
-
-
-document.addEventListener("DOMContentLoaded", (event) => {
+document.addEventListener("DOMContentLoaded", () => {
   const currentYear = new Date().getFullYear();
   document.getElementById("currentyear").textContent = currentYear;
 
   const lastModified = document.lastModified;
-  document.getElementById(
-    "lastModified"
-  ).textContent = `Last Modified: ${lastModified}`;
+  document.getElementById("lastModified").textContent = `Last Modified: ${lastModified}`;
 });
-
-
-//hola
