d56919f7f893562e4e42eb711d9a13038dc64f6a
sollpostacaracciolo <jazzcaracciolo@gmail.com>
2025-09-15 11:11:18 -0300

    p

--- temples.js
+++ temples.js
@@ -1,26 +1,14 @@
-const hamburger = document.getElementById('hamburger');
-const navMenu = document.querySelector('nav ul');
+document.addEventListener("DOMContentLoaded", () => {
+  const hamburger = document.getElementById('hamburger');
+  const navMenu = document.querySelector('nav ul');
 
-hamburger.addEventListener('click', () => {
-  if (navMenu.style.display === 'none' || navMenu.style.display === '') {
-    navMenu.style.display = 'block';
-  } else {
-    navMenu.style.display = 'show';
-  }
-});
+  hamburger.addEventListener('click', () => {
+    navMenu.classList.toggle('show');
+  });
 
-
-
-
-document.addEventListener("DOMContentLoaded", (event) => {
   const currentYear = new Date().getFullYear();
   document.getElementById("currentyear").textContent = currentYear;
 
   const lastModified = document.lastModified;
-  document.getElementById(
-    "lastModified"
-  ).textContent = `Last Modified: ${lastModified}`;
+  document.getElementById("lastModified").textContent = `Last Modified: ${lastModified}`;
 });
-
-
-//hola
