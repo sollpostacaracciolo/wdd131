cc4d7f56914a58c9ad5742b2f0e73641d2a38d58
sollpostacaracciolo <jazzcaracciolo@gmail.com>
2025-10-11 12:59:07 -0300

    s

--- project/scripts/base.js
+++ project/scripts/base.js
@@ -2,12 +2,12 @@ document.addEventListener('DOMContentLoaded', () => {
     const menuButton = document.getElementById('menu');
     const navigation = document.querySelector('.navigation');
     const socialContent = `
-        <a href="https://github.com/demetrioaris" target="_blank" title="GitHub"><img
+        <a href="https://github.com/sollpostacaracciolo" target="_blank" title="GitHub"><img
                 class="icon github-logo" src="../project/images/logos/github-mark-white.png"
                 alt="GitHub icon" /></a>
-        <a href="https://x.com/demetrio_aris" target="_blank" title="X"><img class="icon x-logo"
+        <a href="https://x.com" target="_blank" title="X"><img class="icon x-logo"
                 src="../project/images/logos/logo-x-version-white.png" alt="X logo" /></a>
-        <a href="https://www.linkedin.com/in/d-aris" target="_blank" title="LinkedIn"><img
+        <a href="https://www.linkedin.com/in/mariasolcaracciolo" target="_blank" title="LinkedIn"><img
                 class="icon linkedin-logo" src="../project/images/logos/linkedin-logo-white.png"
                 alt="LinkedIn icon" /></a>
     `;
